617b0fe2f3e68ac262d14996a18f2401732c9a70
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-03-03 17:21:15 -0400

    [Todo]: correct IDs for existing todos in the service

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -5,8 +5,8 @@ import { Todo } from './entity/todo.entity';
 export class TodoService {
   private todos: Todo[] = [
     { id: 1, description: 'Piedra del Alma', done: false },
-    { id: 1, description: 'Piedra del Espacio', done: true },
-    { id: 1, description: 'Piedra del Poder', done: false },
+    { id: 2, description: 'Piedra del Espacio', done: true },
+    { id: 3, description: 'Piedra del Poder', done: false },
   ];
 
   findAll(): Todo[] {
